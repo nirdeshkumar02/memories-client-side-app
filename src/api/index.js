50bce96a02cda8204d127dd850068a6a8a1220dc
Nirdesh Kumar Saini <saini.nirdesh.kumar@gmail.com>
2021-05-18 10:44:01 +0530

    second commit

--- src/api/index.js
+++ src/api/index.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-const API = axios.create({ baseURL: 'https://memories-server-side-app.herokuapp.com/' });
+const API = axios.create({ baseURL: 'https://memories-server-side-app.herokuapp.com' });
 
 API.interceptors.request.use((req) => {
   if (localStorage.getItem('profile')) {
