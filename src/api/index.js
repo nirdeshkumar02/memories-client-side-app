e575b3bba3b8ab8c9cb0e2f01ff3e2321ad2a421
NIRDESH KUMAR SAINI <saini.nirdesh.kumar@gmail.com>
2021-06-26 09:53:01 +0530

    add api

--- src/api/index.js
+++ src/api/index.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-const API = axios.create({ baseURL: 'http://localhost:5000' });
+const API = axios.create({ baseURL: 'https://memories-server-side-app.herokuapp.com' });
 // 'https://memories-server-side-app.herokuapp.com'
 
 API.interceptors.request.use((req) => {
